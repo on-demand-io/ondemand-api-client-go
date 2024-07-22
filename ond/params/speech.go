043c2304b8da366811abdca12082e90ee66878a8
Dinson David Kurian <dinsondavidkurian@gmail.com>
2024-07-22 17:40:28 +0400

    feat: voice and model enums

--- ond/params/speech.go
+++ ond/params/speech.go
@@ -13,7 +13,8 @@ type TextToSpeechParams struct {
 type ServiceModel string
 
 var (
-	ModelTTS1 ServiceModel = "tts-1"
+	ModelTTS1   ServiceModel = "tts-1"
+	ModelTTS1HD ServiceModel = "tts-1-hd"
 )
 
 func (sm ServiceModel) String() string { return string(sm) }
@@ -21,7 +22,12 @@ func (sm ServiceModel) String() string { return string(sm) }
 type ServiceVoice string
 
 var (
-	VoiceAlloy ServiceVoice = "alloy"
+	VoiceAlloy   ServiceVoice = "alloy"
+	VoiceEcho    ServiceVoice = "echo"
+	VoiceFable   ServiceVoice = "fable"
+	VoiceOnyx    ServiceVoice = "onyx"
+	VoiceNova    ServiceVoice = "nova"
+	VoiceShimmer ServiceVoice = "shimmer"
 )
 
 func (v ServiceVoice) String() string { return string(v) }
